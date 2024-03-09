af43e01e07f90f58de20dedff0490813c0c7680a
hector Govin <hectorgovin@outlook.com>
2024-03-08 18:35:39 -0600

    Se terminó el diseño de la pestaña Cobro Cliente Frecuente. Y también se terminó el diseño de la pestaña Cobro Rápido.
    Pendiente: funcionabilidad.

---
+++ src/MenuPrincipal/MenuPrincipalCobroRapido.java
@@ -0,0 +1,276 @@
+package MenuPrincipal;
+import java.sql.*;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class MenuPrincipalCobroRapido extends javax.swing.JFrame {
+    
+    public JTable getTablaPRODUCTOS(){
+        return tablaPRODUCTOS;
+    }
+    
+    private AgregarProductos agregarProductos;
+    
+    public void actualizar(){
+        DefaultTableModel model = (DefaultTableModel)tablaPRODUCTOS.getModel();
+        model.setRowCount(0);
+    }
+    
+    public String[][] PRODUCTOS;
+    String[][] data = new String[9][9];
+    
+    public static final String URL = "jdbc:mysql://localhost:3306/sebd";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+    
+
+    public MenuPrincipalCobroRapido() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        mostrarProductos();
+    }
+    
+    public void mostrarProductos(){
+        Productos objetoProductos = new Productos();
+        objetoProductos.MostrarProductos(tablaPRODUCTOS);
+    }
+
+    public static Connection getConection(){
+        
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return con;
+    }
+    
+        public void eliminar(){
+        int row = tablaPRODUCTOS.getSelectedRow();
+        String value = tablaPRODUCTOS.getModel().getValueAt(row, 0).toString();
+        
+        Productos ObjetoProductos = new Productos();
+        ObjetoProductos.EliminarProducto(value);
+    }
+
+    
+    
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jPanel_Fondo = new javax.swing.JPanel();
+        jPanel_Barra = new javax.swing.JPanel();
+        jLabel_RegistroProd = new javax.swing.JLabel();
+        jLabel_Folio_S = new javax.swing.JLabel();
+        jLabel_Folio = new javax.swing.JLabel();
+        jPanel_Opciones = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tablaPRODUCTOS = new javax.swing.JTable();
+        jButton_Cancelar = new javax.swing.JButton();
+        jButton_Confirmar = new javax.swing.JButton();
+
+        setLocation(new java.awt.Point(0, 0));
+
+        jPanel_Fondo.setBackground(new java.awt.Color(243, 255, 242));
+
+        jPanel_Barra.setBackground(new java.awt.Color(153, 188, 133));
+
+        jLabel_RegistroProd.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel_RegistroProd.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel_RegistroProd.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel_RegistroProd.setText("Cobro Rápido");
+
+        jLabel_Folio_S.setFont(new java.awt.Font("Segoe UI", 1, 15)); // NOI18N
+        jLabel_Folio_S.setText("FOLIO:");
+
+        jLabel_Folio.setFont(new java.awt.Font("Segoe UI", 1, 15)); // NOI18N
+        jLabel_Folio.setText("0001");
+
+        javax.swing.GroupLayout jPanel_BarraLayout = new javax.swing.GroupLayout(jPanel_Barra);
+        jPanel_Barra.setLayout(jPanel_BarraLayout);
+        jPanel_BarraLayout.setHorizontalGroup(
+            jPanel_BarraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_BarraLayout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(jLabel_RegistroProd)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel_Folio_S, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel_Folio)
+                .addGap(16, 16, 16))
+        );
+        jPanel_BarraLayout.setVerticalGroup(
+            jPanel_BarraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_BarraLayout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addGroup(jPanel_BarraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel_RegistroProd)
+                    .addGroup(jPanel_BarraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel_Folio_S, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel_Folio)))
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+
+        jPanel_Opciones.setBackground(new java.awt.Color(243, 255, 242));
+        jPanel_Opciones.setPreferredSize(new java.awt.Dimension(1280, 720));
+
+        tablaPRODUCTOS.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        tablaPRODUCTOS.getTableHeader().setReorderingAllowed(false);
+        tablaPRODUCTOS.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tablaPRODUCTOSMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tablaPRODUCTOS);
+
+        jButton_Cancelar.setText("Cancelar");
+        jButton_Cancelar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton_CancelarMouseClicked(evt);
+            }
+        });
+
+        jButton_Confirmar.setText("Confirmar");
+        jButton_Confirmar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton_ConfirmarMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel_OpcionesLayout = new javax.swing.GroupLayout(jPanel_Opciones);
+        jPanel_Opciones.setLayout(jPanel_OpcionesLayout);
+        jPanel_OpcionesLayout.setHorizontalGroup(
+            jPanel_OpcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_OpcionesLayout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 461, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(jPanel_OpcionesLayout.createSequentialGroup()
+                .addGap(75, 75, 75)
+                .addComponent(jButton_Confirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(92, 92, 92)
+                .addComponent(jButton_Cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel_OpcionesLayout.setVerticalGroup(
+            jPanel_OpcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_OpcionesLayout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 346, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel_OpcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton_Confirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton_Cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(12, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel_FondoLayout = new javax.swing.GroupLayout(jPanel_Fondo);
+        jPanel_Fondo.setLayout(jPanel_FondoLayout);
+        jPanel_FondoLayout.setHorizontalGroup(
+            jPanel_FondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel_Barra, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel_FondoLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel_Opciones, javax.swing.GroupLayout.PREFERRED_SIZE, 461, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel_FondoLayout.setVerticalGroup(
+            jPanel_FondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_FondoLayout.createSequentialGroup()
+                .addComponent(jPanel_Barra, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel_Opciones, javax.swing.GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel_Fondo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel_Fondo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton_CancelarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton_CancelarMouseClicked
+    this.dispose();
+    }//GEN-LAST:event_jButton_CancelarMouseClicked
+
+    private void tablaPRODUCTOSMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaPRODUCTOSMouseClicked
+     
+    }//GEN-LAST:event_tablaPRODUCTOSMouseClicked
+
+    private void jButton_ConfirmarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton_ConfirmarMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton_ConfirmarMouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipalCobroRapido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipalCobroRapido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipalCobroRapido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipalCobroRapido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MenuPrincipalCobroRapido().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_Cancelar;
+    private javax.swing.JButton jButton_Confirmar;
+    private javax.swing.JLabel jLabel_Folio;
+    private javax.swing.JLabel jLabel_Folio_S;
+    private javax.swing.JLabel jLabel_RegistroProd;
+    private javax.swing.JPanel jPanel_Barra;
+    private javax.swing.JPanel jPanel_Fondo;
+    private javax.swing.JPanel jPanel_Opciones;
+    private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JTable tablaPRODUCTOS;
+    // End of variables declaration//GEN-END:variables
+}
